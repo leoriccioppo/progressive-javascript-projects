e99c3a43af034a21f9fddb320f6b8ef73f860976
Leonora Riccioppo <119987608+leoriccioppo@users.noreply.github.com>
2023-07-10 18:05:31 -0300

    alteracoes

--- src/scripts/tic-tac-toeGame.js
+++ src/scripts/tic-tac-toeGame.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+// Variáveis do jogo
+let currentPlayer = 'X';
+let secondPlayer = 'O';
+let gameActive = true;
+
+//referencias HTML
+const status = document.getElementsByClassName ('status');
+
+const cells = document.querySelectorAll('.cell');
+
+const restart = document.getElementsByClassName('restart');
+
+cells.forEach(cell=>{
+    cell.addEventListener('click', handleCLick);
+})
