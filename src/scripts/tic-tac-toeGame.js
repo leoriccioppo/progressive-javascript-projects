f2dfcac0cfc9eee30805f7bcbf3a1301aa021c6b
Leonora Riccioppo <119987608+leoriccioppo@users.noreply.github.com>
2023-07-10 22:07:14 -0300

    edit

--- src/scripts/tic-tac-toeGame.js
+++ src/scripts/tic-tac-toeGame.js
@@ -1,22 +1,37 @@
 // Variáveis do jogo
-let currentPlayer = 'X';
-let secondPlayer = 'O';
+let currentPlayer;
 let gameActive = true;
 
 //referencias HTML
 const status = document.getElementsByClassName ('status');
 
-const celElements = document.querySelectorAll("['data-cel']");
+const celElements = document.querySelectorAll('data-cel');
 
 const restart = document.getElementsByClassName('restart');
 
-//exibir a jogador na tela
-const avatarPlayer = (cel, classToAdd) =>{
-    cel.classList.add(classToAdd);
+//combinações vencendoras
+const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+//selecionar primeiro player 
+function selectPlayer() {
+    const player1 = document.querySelector('input[name="player1"]:checked').value;
+    currentPlayer = player1;   
+    
+    //inicia a tela
+    const button = document.querySelector('button');
+    button.addEventListener('click',()=>{
+        document.getElementById('message').style.display = 'none';
+        document.getElementById('game').style.display = 'block';
+    })
+    console.log(player1);
 }
 
-
-
-celElements.forEach(cel=>{
-    cel.addEventListener('click', handleCLick, {once: true});
-})
