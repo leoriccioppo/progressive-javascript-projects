9ecc862aac25b6e1e9d832a26505e64d7da56eca
Leonora Riccioppo <119987608+leoriccioppo@users.noreply.github.com>
2023-07-19 19:52:00 -0300

    variaveis&referencias

--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -1,1 +1,18 @@
-//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=163db620f1c61a0cc271e50a7f60976a
+const apiKey = "163db620f1c61a0cc271e50a7f60976a";
+const apiCountryFlagUrl = "https://www.countryflagicons.com/";
+
+//form
+const cityInput = document.querySelector('#city-input');
+const searchButton = document.querySelector('#search');
+//weather data
+const cityElement = document.querySelector('#city');
+const countryflagicons = document.querySelector('#country');
+const temperatureElement = document.querySelector('#temperature');
+//description
+const descriptionElement = document.querySelector('#description');
+const humdityElement = document.querySelector('#humidity');
+const windElement = document.querySelector('#wind');
+
+searchButton.addEventListener('click', () => {
+    console.log('search');
+})
