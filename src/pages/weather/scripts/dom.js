0c1386c3a50ae7055dba46a5b602958c2c1f8291
Leonora Riccioppo <119987608+leoriccioppo@users.noreply.github.com>
2023-07-28 23:34:36 -0300

    add

--- src/pages/weather/scripts/dom.js
+++ src/pages/weather/scripts/dom.js
@@ -1,9 +1,11 @@
 //background
 export const pageWeather = document.querySelector('#pageWeather');
+export const screen = document.querySelector('#screen');
 //form
 export const cityInput = document.querySelector('#city-input');
 export const searchButton = document.querySelector('#search');
 //weather data
+export const weatherDataElement = document.querySelector('.weather-data');;
 export const cityElement = document.querySelector('#city');
 export const countryFlagIcons = document.querySelector('#country');
 export const temperatureElement = document.querySelector('#temperature');
--- src/pages/weather/scripts/helpers.js
+++ src/pages/weather/scripts/helpers.js
@@ -1,4 +1,4 @@
-import { pageWeather, cityElement, countryFlagIcons, temperatureElement, descriptionElement, weatherElement, humidityElement, windElement } from './dom.js';
+import { pageWeather, screen, cityElement, countryFlagIcons, temperatureElement, descriptionElement, weatherElement, humidityElement, windElement, weatherDataElement } from './dom.js';
 
 //construindo imagens
 export function addImageToContainer(containerId, imageUrl, altText) {
@@ -17,6 +17,8 @@ export function addImageToContainer(containerId, imageUrl, altText) {
 //muda background de acordo com a cidade
 export function changeBackground(city) {
     const imageUrl = `https://source.unsplash.com/1600x900/?${city}`;
-    pageWeather.classList.add('background');
     pageWeather.style.backgroundImage = `url('${imageUrl}')`;
+    pageWeather.classList.add('background');
+    weatherDataElement.classList.remove('hidden');
+    screen.classList.add('fundo');
   }
